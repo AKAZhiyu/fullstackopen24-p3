b092a44f6e3550f3918ef3244775ac0004eb6a52
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-01-28 10:08:31 +0800

    finish exercise 3.13

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
+const Person = require('./models/person')
 const app = express()
 
-morgan.token('obj', (req, res) => JSON.stringify(req.body) )
+morgan.token('obj', (req, res) => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(cors())
@@ -12,68 +14,41 @@ app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :obj'));
 
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    },
-    {
-        "id": "5",
-        "name": "Raymond Wang",
-        "number": "1314520wmr"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
-    const currentDate = new Date(); // 获取当前日期和时间
-    const formattedDate = currentDate.toString(); // 格式化日期
-    response.send(`
-        <p>The phonebook has info for ${persons.length} people</p>
-        <p>${formattedDate}</p>
-        `)
+    const currentDate = new Date(); 
+    const formattedDate = currentDate.toString(); 
+    const count = Person.countDocuments().then(count => {
+        response.send(`
+            <p>The phonebook has info for ${count} people</p>
+            <p>${formattedDate}</p>
+            `)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = persons.find(p => p.id === id)
-
-    if (!person) {
-        return response.status(404).end()
-    }
-
-    response.json(person)
+    Person.findById(id).then(person => {
+        response.json(person)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(id).then(person => {
+        if (person) {
+            response.status(204).end()
+        } else {
+            response.status(404).json({ error: "Person not found" })
+        }
+    })
 })
 
-const generateId = () => {
-
-    return String(Math.floor(Math.random() * 1000000))
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -84,21 +59,21 @@ app.post('/api/persons', (request, response) => {
         });
     }
 
-    if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({
-            error: "Name must be unique"
+    Person.findOne({name: body.name}).then(existingPerson => {
+        if (existingPerson) {
+            return response.status(400).json({
+                error: "Name must be unique"
+            })
+        }
+
+        const person = new Person({
+            name: body.name,
+            number: body.number,
         });
-    }
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+        return person.save()
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    }).then(savedPerson => response.json(savedPerson))
 })
 
 
