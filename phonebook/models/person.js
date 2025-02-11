c317b922884128e7a0ec61e2c2fc81374deb78b6
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-02-11 11:43:26 +0800

    finish exercise 3.20

--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -15,20 +15,29 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: 5,
-      required: true
-    },
-    number: String,
+  name: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: [8, "Number should have a length of 8 or more"],
+    validate: {
+      validator: (number) => {
+        return /^(?=.{8,}$)\d{2,3}-\d+$/.test(number)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    }
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObj) => {
-        returnedObj.id = document._id.toString()
-        delete returnedObj._id
-        delete returnedObj.__v
-    }
+  transform: (document, returnedObj) => {
+    returnedObj.id = document._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
